89563d7b9634a3eb09d55f80f1e25a54db6e4200
@GrizzlyKamen <=>
2025-02-11 11:14:48 -0600

    Creation of variables and assigning them numeric values to do arithmetic operations

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 29;
+let secondNumber = 31;
+
+let resultAddition = firstNumber + secondNumber;
+let resultSubstract = secondNumber - firstNumber;
+let resultMultiply = firstNumber * secondNumber;
+let resultDivision = firstNumber / secondNumber;
+
+console.log(resultAddition);
+console.log(resultSubstract);
+console.log(resultMultiply);
+console.log(resultDivision);
